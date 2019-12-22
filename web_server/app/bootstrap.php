171089b24397625b2b82c3009cf8b1b2922eeb5d
RyosukeSomeya <argama.someya@gmail.com>
2019-12-22 22:54:54 +0900

    インデント修正

--- web_server/app/bootstrap.php
+++ web_server/app/bootstrap.php
@@ -5,9 +5,9 @@
  *
  * */
 
-    require 'core/ClassLoader.php';
+require 'core/ClassLoader.php';
 
-    $loader = new ClassLoader();
-    $loader->registerDir(dirname(__FILE__) . '/core');
-    $loader->registerDir(dirname(__FILE__) . '/models');
-    $loader->register();
+$loader = new ClassLoader();
+$loader->registerDir(dirname(__FILE__) . '/core');
+$loader->registerDir(dirname(__FILE__) . '/models');
+$loader->register();
