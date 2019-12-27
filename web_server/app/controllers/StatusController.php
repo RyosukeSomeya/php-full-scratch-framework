1d4e2c2885b3e5a514782060164852db6b4635d4
RyosukeSomeya <rx.78.0618@gmail.com>
2019-12-28 02:38:58 +0900

    テストコミット

--- web_server/app/controllers/StatusController.php
+++ web_server/app/controllers/StatusController.php
@@ -5,8 +5,7 @@ class StatusController extends Controller
     public function indexAction()
     {
         $user = $this->session->get('user');
-        $statuses = $this->db_manager->get('Status')
-            ->fetchAllPersonalArchivesByUserId($user['id']);
+        $statuses = $this->db_manager->get('Status')->fetchAllPersonalArchivesByUserId($user['id']);
 
         return $this->render(array(
             'statuses' => $statuses,
