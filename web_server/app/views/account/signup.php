17d0243a13d43fbb637bb931c74a5c97a0f56e80
RyosukeSomeya <rx.78.0618@gmail.com>
2019-12-28 22:37:06 +0900

    Viewfileを分離

--- web_server/app/views/account/signup.php
+++ web_server/app/views/account/signup.php
@@ -4,26 +4,11 @@
 <form action="<?php echo $base_url; ?>/account/register" method="post">
     <input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>">
     <?php if (isset($errors) && count($errors) > 0): ?>
-    <ul class="error_list">
-        <?php foreach ($errors as $error): ?>
-        <li><?php echo $this->escape($error); ?></li>
-        <?php endforeach; ?>
-    </ul>
+    <li><?php echo $this->render('errors', array('errors' => $errors)); ?></li>
     <?php endif; ?>
-    <table>
-        <tr>
-        <th>ユーザーID</th>
-        <td>
-            <input type="text" name="user_name" value="<?php echo $this->escape($user_name); ?>">
-        </td>
-        <tr>
-            <th>パスワード</th>
-            <td>
-                <input type="password" name="password" value="<?php echo $this->escape($password); ?>">
-            </td>
-        </tr>
-        </tr>
-    </table>
-
+    <?php echo $this->render('account/inputs', array(
+        'user_name' => $user_name, 'password' => $password,
+    )); ?>
+    <?php var_dump($this); ?>
     <p><input type="submit" value="登録"></p>
 </form>
